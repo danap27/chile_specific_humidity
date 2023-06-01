da21f27a33714376f41605da1ecd96dcc2f41ee4
David Navarrete <davinavarrete@alumnos.uai.cl>
2023-06-01 15:12:33 -0400

    first commit

---
+++ utils.py
@@ -0,0 +1,31 @@
+import os
+
+
+def sort_model_files_by_folder(data_path):
+    """
+    Separa los archivos .nc en carpetas por modelo.
+    """
+    # Listamos los archivos que hay en el directorio con extension .nc
+    files = [f for f in os.listdir(data_path) if f.endswith('.nc')]
+    if len(files) == 0:
+        print('No hay archivos .nc en el directorio.')
+        return
+    # Iteramos sobre los archivos
+    for f in files:
+        # Separamos los nombres de los archivos por el caracter '_'
+        # y nos quedamos con todos los elementos menos el ultimo
+        # que es la extension .nc
+        name = f.split('_')[:-1]
+        # Unimos los elementos de la lista con el caracter '_'
+        # para obtener el nombre del modelo
+        model = '_'.join(name)
+        # Creamos el directorio del model si no existe
+        if not os.path.exists(os.path.join(data_path, model)):
+            os.makedirs(os.path.join(data_path, model))
+        # Movemos el archivo a la carpeta correspondiente
+        os.rename(os.path.join(data_path, f), os.path.join(data_path, model, f))
+        print(f'Archivo {f} movido a {model}')
+    print('Archivos movidos correctamente.')
+
+sort_model_files_by_folder('proyecto/cordex_models')
+
